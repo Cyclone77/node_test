4cd00ca8a7263d54e42f1cfa47447e573fa6e78a
cyclone77 <fillchar@live.com>
2019-02-26 21:11:34 +0800

    构建sql

--- 4.http/3.mssql.js
+++ 4.http/3.mssql.js
@@ -4,9 +4,36 @@ const sql = require('mssql')
 
 const data = require('./data.json')
 
-data.forEach(item => {
+const _arr = [];
 
-});
+function _buildDataArray(_res, parent) {
+    _res.forEach(item => {
+        _arr.push({
+            id: item.id,
+            text: item.text,
+            hasChildren: (item.childNodes && item.childNodes.length > 0),
+            parent: parent
+        })
+
+        if (item.childNodes && item.childNodes.length > 0) {
+            _buildDataArray(item.childNodes, item.id);
+        }
+    });
+}
+
+_buildDataArray(data, '.');
+
+const _sqlList = [];
+
+function _buildSQLList() {
+    _arr.forEach(item => {
+        const _sqltemp = `INSERT INTO #Course_Tree(id, text, hasChildren, parent) VALUES('${item.id}', '${item.text}', '${item.hasChildren}', '${item.parent}')`;
+        _sqlList.push(_sqltemp);
+    })
+}
+
+_buildSQLList();
+console.dir(_sqlList);
 
 const config = {
     user: 'sa',
